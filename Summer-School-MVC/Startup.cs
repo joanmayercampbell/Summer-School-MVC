39dcc0493e160522158eaac4f53977c16e3e6f1f
Joan Mayer Campbell <joanmayercampbell@gmail.com>
2016-07-11 15:38:32 -0400

    initial commit

---
+++ Summer-School-MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Summer_School_MVC.Startup))]
+namespace Summer_School_MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
